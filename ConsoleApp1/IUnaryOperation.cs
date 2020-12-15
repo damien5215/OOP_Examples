4be9ee17629e849267e86671b5413f80ca2cbc29
damien5215 <37832934+damien5215@users.noreply.github.com>
2020-12-15 17:26:45 +0000

    Added Interface example
    IUnaryOperation and class Increment.

---
+++ ConsoleApp1/IUnaryOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IUnaryOperation
+    {
+        double Perform(double value);
+    }
+    class Increment : IUnaryOperation
+    {
+        public double Perform(double value)
+        {
+            return (value + 1);
+        }
+    }
+}
+
+//Increment inc = new Increment();
+//Console.WriteLine(inc.Perform(5));
