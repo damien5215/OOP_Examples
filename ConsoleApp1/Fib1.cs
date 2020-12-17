8c27f4be5e5e872fa9861ea363ea4c157e000117
damien5215 <37832934+damien5215@users.noreply.github.com>
2020-12-17 20:23:07 +0000

    Added Fibonacci calculator
    Added Fibonacci calculator (Dynamic).

---
+++ ConsoleApp1/Fib1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Fib1
+    {
+        public static int Fib(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else 
+            {
+                int[] fibArray = new int[n];
+                fibArray[0] = 0;
+                fibArray[1] = 1;
+                n--;
+
+                return calcFib(n, fibArray);
+            }
+        }
+
+        public static int calcFib(int n, int[] fibArray)
+        {
+            if (n < 2)
+            {
+                return 1;  // return n
+            }
+            else if (fibArray[n] != 0)
+            {
+                return fibArray[n];
+            }
+            else
+            {
+                fibArray[n] = calcFib(n - 1, fibArray) + calcFib(n - 2, fibArray);
+                return fibArray[n];
+            }
+        }
+    }
+}
+
+//int n1 = 46;
+
+//Console.WriteLine("");
+//Console.WriteLine("The " + n1 + "th term of the fibonacci sequence is: " + Fib1.Fib(n1));
+//Console.WriteLine("");
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int n1 = 46;
 
+            Console.WriteLine("");
+            Console.WriteLine("The " + n1 + "th term of the fibonacci sequence is: " + Fib1.Fib(n1));
+            Console.WriteLine("");
         }
     }
 }
+
+// n = 46, result = 1836311903
+// n = 47, result = 2971215073
+
+// Signed 32-bit integer goes up to 2,147,483,647
