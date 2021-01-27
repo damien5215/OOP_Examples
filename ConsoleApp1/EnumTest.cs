64b67243e7c916a77f648d57046ebe0cba787bba
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-01-27 22:37:09 +0000

    Added EnumTest

---
+++ ConsoleApp1/EnumTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EnumTest
+    {
+        public enum IntensityLevel
+        {
+            Low = 1,
+            Medium = 2,
+            High = 3
+        }
+
+    }
+}
+
+//var results = Enum.GetValues(typeof(EnumTest.IntensityLevel))
+//    .Cast<EnumTest.IntensityLevel>()
+//    .Select(il => new { id = (int)il, name = il.ToString() })
+//    .ToList();
+
+//Console.WriteLine(results[0]);
+//Console.WriteLine(results[1]);
+//Console.WriteLine(results[2]);
