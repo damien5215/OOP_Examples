c8fe158e9cca5e907446b515db0aba16644b3762
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-01-29 18:28:17 +0000

    Added LamdaEx

---
+++ ConsoleApp1/LamdaEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LamdaEx
+    {
+        public static IEnumerable<int> Square(int[] input) 
+        {
+            var squaredNumbers = input.Select(x => x * x);
+
+            return squaredNumbers;
+        } 
+
+    }
+}
+
+//int[] numbers = { 2, 3, 4, 5 };
+
+//var result = LamdaEx.Square(numbers);
+
+//foreach (int number in result) 
+//{
+//    Console.WriteLine(number);
+//}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,15 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //FibConsole.MenuFunction();
+            int[] numbers = { 2, 3, 4, 5 };
+
+            var result = LamdaEx.Square(numbers);
+
+            foreach (int number in result)
+            {
+                Console.WriteLine(number);
+            }
 
         }
     }
 }
 
 //FibConsole.MenuFunction();
-//int[] arr = { 17, 2, 34, 48, 25 };
 
-//int[] arr = {3, 6, 2, 5};
+//int[] arr = { 17, 2, 34, 48, 25 };
 //Bubble bubble1 = new Bubble();
 //bubble1.Sort2(arr);
