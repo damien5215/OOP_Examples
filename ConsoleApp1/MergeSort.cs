829a28b030c52c1527444dd3672097279159140c
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-02-15 23:22:24 +0000

    General Refactor

--- ConsoleApp1/MergeSort.cs
+++ ConsoleApp1/MergeSort.cs
@@ -12,7 +12,6 @@ namespace ConsoleApp1
         public static int[] sort(int[] input) 
         {
             //List<int> digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
             List<int> inputList = input.ToList(); //new List<int>();
 
             List<int> Left;
