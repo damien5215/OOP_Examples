8aab0f7e092d60a97c1ecafa623bfe5036427a1b
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-04-01 16:39:33 +0100

    General Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            FibConsole.MenuFunction();
+            int a = 7; int b = 9; int c = 7;
+            MathOps math1 = new MathOps();
 
+            Console.WriteLine("Square Function " + c + " = " + 
+                               math1.MathOperation(c));
+
+            Console.WriteLine("Multiply Function (" + a + ", " + b + ") = " + 
+                              math1.MathOperation(a, b));
         }
     }
 }
