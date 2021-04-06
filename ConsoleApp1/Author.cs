dd923bed7c04969341e955b99e8c593a1b4b0d15
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-04-06 16:41:15 +0100

    Added Example

---
+++ ConsoleApp1/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Author
+    {
+        public string Name = "Ernest Hemingway";
+    }
+
+    class Book : Author
+    {
+        public string Title = "A Moveable Feast";
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,14 +11,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int a = 7; int b = 9; int c = 7;
-            MathOps math1 = new MathOps();
-
-            Console.WriteLine("Square Function " + c + " = " + 
-                               math1.MathOperation(c));
-
-            Console.WriteLine("Multiply Function (" + a + ", " + b + ") = " + 
-                              math1.MathOperation(a, b));
+            Book book1 = new Book();
+            Console.WriteLine(book1.Name);
+            Console.WriteLine(book1.Title);
         }
     }
 }
