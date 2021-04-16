449ba117ecd3b6771950d9fbcdc7ec9e13a80d8f
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-04-16 12:33:24 +0100

    General Refactor

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,9 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Pikachu pika = new Pikachu();
-            pika.pokemonSound();
-            pika.attackList();
+            FibConsole.MenuFunction();
 
         }
     }
