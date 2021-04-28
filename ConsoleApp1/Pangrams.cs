ace5cafa384ff5fa7a38f2f33830718a526fc4f3
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-04-28 23:10:15 +0100

    General Update

--- ConsoleApp1/Pangrams.cs
+++ ConsoleApp1/Pangrams.cs
@@ -34,4 +34,9 @@ namespace ConsoleApp1
 
 //string s1 = "We promptly judged antique ivory buckles for the next prize";
 //string s2 = "The quick brown fox jumps over the lazy dog";
+//string s3 = "abcd efg hijk lmnop qrs tuv";
+//string s4 = "abcd efg hijk lmnop qrs tuv wxyz";
 //Pangrams.Pangram(s1);
+//Pangrams.Pangram(s2);
+//Pangrams.Pangram(s3);
+//Pangrams.Pangram(s4);
