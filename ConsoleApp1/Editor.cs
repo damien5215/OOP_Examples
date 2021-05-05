011d0b2aa979d6da6a6ad881ff7ad9780a6438d1
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-05-05 23:28:37 +0100

    General Update

---
+++ ConsoleApp1/Editor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Editor
+    {
+        public string WordCount(string input) 
+        {
+            //input = "one two. three. four five six. seven. eight nine.";
+
+            int totalWords = 0;
+            string splitOn = ".";
+
+            string result = input.Replace('?', '.').Replace('!', '.').Replace(". ", ".");
+
+
+            string[] sentenceArray = result.Split(splitOn.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            int[] wordArr = new int[(sentenceArray.Length)];
+
+            for (int i = 0; i < sentenceArray.Length; i++)
+            {
+                wordArr[i] = sentenceArray[i].Split(' ').Length;
+                totalWords += wordArr[i];
+                result += "Sentence " + (i + 1) + " contains " + wordArr[i] + " words." + "\n";
+            }
+
+            return result;
+        }
+    }
+}
