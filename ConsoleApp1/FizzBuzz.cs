bf1fec3465e518ffb00872d3152d7beae39339c2
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-05-12 23:46:45 +0100

    General Update

---
+++ ConsoleApp1/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class FizzBuzz
+    {
+        public void PrintFizzBuzz(int input)
+        {
+
+            Console.WriteLine(input);
+
+            //int result = input.ToCharArray().Length;
+            //return result;
+        }
+    }
+}
