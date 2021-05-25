c252326beef9aef1030392a162a3695c878a64f0
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-05-26 00:33:19 +0100

    General Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,30 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] arr0 = {2, 3, 1};
-            int[] arr1 = new int[arr0.Length]; //= {1, 2, 3};
+            FibConsole.MenuFunction();
 
-            int[] arr2 = {0, 0, 0};
-            int[] arr3 = {0, 0, 0};
-
-            for (int i = 0; i < arr0.Length; i++) 
-            {
-                arr1[i] = i + 1;
-            }
-
-            for (int i = 0; i < arr0.Length; i++) 
-            {
-                arr2[i] = Array.IndexOf(arr0, arr1[i]) + 1;
-                Console.Write(arr2[i] + " ");
-            }
-
-            Console.WriteLine("\n");
-
-            for (int i = 0; i < arr1.Length; i++)
-            {
-                arr3[i] = Array.IndexOf(arr0, arr2[i]) + 1;
-                Console.Write(arr3[i] + " ");
-            }
         }
     }
 }
