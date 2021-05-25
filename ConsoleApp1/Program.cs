b0d7dbbe243ee692f47a3b9439c7266d36f1c9fd
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-05-25 01:50:43 +0100

    General Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            FibConsole.MenuFunction();
+            //FibConsole.MenuFunction();
 
+            int count1 = 0;
+            int count2 = 0;
+            int result = 0;
+
+            List<int> nums = new List<int>() { 1, 2, 2, 3};
+
+            int[] arr = nums.ToArray();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count1++;
+
+                    if (count1 > count2)
+                    {
+                        count2 = count1;
+                        //index = i;
+                        result = arr[i];
+                    }
+                }
+
+                else
+                {
+                    count1 = 0;
+                }
+            }
+            Console.WriteLine(result);
         }
     }
 }
